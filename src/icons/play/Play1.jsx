b2a3a03b7ff04ad512a7430ab3a3fb2e3c70f059
Santoshhhhhhh <107632207+Santoshhhhhhh@users.noreply.github.com>
2024-06-30 20:50:14 +0500

    Add files via upload

---
+++ src/icons/play/Play1.jsx
@@ -0,0 +1,25 @@
+/*
+We're constantly improving the code you see. 
+Please share your feedback here: https://form.asana.com/?k=uvp-HPgd3_hyoXRBw1IcNg&d=1152665201300829
+*/
+
+import React from "react";
+
+export const Play1 = ({ className }) => {
+  return (
+    <svg
+      className={`play-1 ${className}`}
+      fill="none"
+      height="24"
+      viewBox="0 0 24 24"
+      width="24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        className="path"
+        d="M2.28408 6.42515L11.7633 18.3084C11.92 18.5046 12.2515 18.5046 12.4083 18.3084L21.8885 6.42432C22.0014 6.28219 22.0348 6.07033 21.9739 5.88591C21.9613 5.84754 21.9473 5.81783 21.9356 5.79597C21.8634 5.66188 21.7241 5.57812 21.5717 5.57812L2.59991 5.57812C2.44808 5.57812 2.30822 5.66167 2.23643 5.79596C2.22405 5.81845 2.21023 5.84816 2.19827 5.88384C2.13741 6.07012 2.17063 6.2826 2.28408 6.42515Z"
+        fill="white"
+      />
+    </svg>
+  );
+};
